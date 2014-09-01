7c06b5703ea5c9814f1abbcae700567ca4e72889
Kim D Barnes <kimdbarnes@gmail.com>
2014-09-01 17:48:18 -0600

    David, please make me pretty like your hair

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,13 +1,62 @@
 class CodeMaker
   attr_reader :code
 
+  CODE_LENGTH = 4
+
   def initialize(code = nil)
     @code = code || generate_code
+  end
+
+  def correct?(guess)
+    guess.downcase == code
+  end
+
+  def score(guess)
+    Score.new(code, guess).evaluate
   end
 
   private
 
   def generate_code
-    'rgby'
+    colors = ['r', 'g', 'y', 'b']
+    (1..CODE_LENGTH).reduce('') do |result, _|
+      result + colors.sample
+    end
+  end
+
+  class Score
+    def initialize(code, guess)
+      @code = code
+      @guess = guess
+    end
+
+    def evaluate
+      [correct_locations_count, correct_colors_count]
+    end
+
+    private
+
+    attr_reader :code, :guess
+
+    def correct_locations_count
+      CODE_LENGTH - non_exact_matches.count
+    end
+
+    def non_exact_matches
+      code.chars.zip(guess.downcase.chars).select { |a, b| a !=b }
+    end
+
+    def correct_colors_count
+      return 0 if correct_locations_count == CODE_LENGTH
+
+      (remaining_code, remaining_guess) = non_exact_matches.transpose
+      remaining_code = remaining_code.join('')
+
+      remaining_guess.count do |guess_char|
+        match = remaining_code.include? guess_char
+        remaining_code = remaining_code.sub(guess_char, '')
+        match
+      end
+    end
   end
 end
--- spec/code_maker_spec.rb
+++ spec/code_maker_spec.rb
@@ -27,6 +27,81 @@ describe CodeMaker do
       CodeMaker.new.code
     end
 
-    lots_of_codes.uniq.length.should >= 80
+    lots_of_codes.uniq.length.should >= 60
+  end
+
+  describe '#correct?' do
+    it 'should return true' do
+      sut = CodeMaker.new('rrrr')
+      expect(sut.correct?('RRRR')).to eq(true)
+    end
+
+    it 'should return false' do
+      sut = CodeMaker.new('rrrr')
+      expect(sut.correct?('RRRB')).to eq(false)
+    end
+  end
+
+  describe '#score' do
+    describe 'correct locations count' do
+      it 'should return 4 when all the right colors are in the right place' do
+        sut = CodeMaker.new('rrrr')
+        expect(sut.score('rrrr').first).to eq 4
+      end
+
+      it 'should return 4 even when guess is not lowercase' do
+        sut = CodeMaker.new('rrrr')
+        expect(sut.score('RRRR').first).to eq 4
+      end
+
+      it 'should return 1 when 1 color is in the right place' do
+        sut = CodeMaker.new('rrrr')
+        expect(sut.score('rbbb').first).to eq 1
+      end
+
+      it 'should return 0 when no colors are in the right place' do
+        sut = CodeMaker.new('rrrr')
+        expect(sut.score('bbbb').first).to eq 0
+      end
+    end
+
+    describe 'correct colors count' do
+      it 'should work perfectly under the right conditions' do
+        sut = CodeMaker.new('rrrr')
+        expect(sut.score('bbbb').last).to eq 0
+      end
+
+      it 'should return 1 when 1 color is not in the right place' do
+        sut = CodeMaker.new('rbbb')
+        expect(sut.score('bggg').last).to eq 1
+      end
+
+      it 'should return 2 when 2 colors match but are not in the right place' do
+        sut = CodeMaker.new('rrgg')
+        expect(sut.score('yyrr').last).to eq 2
+      end
+
+      it 'should return 2 when 2 colors match and are in the right place' do
+        sut = CodeMaker.new('rrgg')
+        expect(sut.score('rryy').last).to eq 0
+      end
+    end
+
+    it 'should return 2 and 2 when two are in the right location and 2 are the right colors' do
+      sut = CodeMaker.new('rgby')
+      expect(sut.score('rgyb')).to eq [2, 2]
+    end
+
+    it 'should return 1 and 1 when there are duplicates in the guess' do
+      sut = CodeMaker.new('rrbb')
+      expect(sut.score('rgrr')).to eq [1, 1]
+    end
+
+    context 'uppercase' do
+      it 'should return 2 and 2 when two are in the right location and 2 are the right colors' do
+        sut = CodeMaker.new('rgby')
+        expect(sut.score('RGYB')).to eq [2, 2]
+      end
+    end
   end
 end
